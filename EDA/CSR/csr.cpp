44f6fdfafaab02b28f3bf00603899dfa24b7bf90
Pedro <pedrocanddz07@gmail.com>
2022-06-22 12:03:07 -0300

    at1 note

--- EDA/CSR/csr.cpp
+++ EDA/CSR/csr.cpp
@@ -7,16 +7,27 @@ typedef struct{
 }matrix;
 
 void printaCSR(int a[], int b[], int c[], int k);
-
+void ordenaMatrix(matrix a[], int k);
 int main ()
 {   
     int k, col, row, dado;
     cin >> k;
     int a[100000], b[100000], c[100000]= {0};
-    int d[100000]= {0};
     matrix *m = (matrix *)malloc(sizeof(matrix) * k);
     
-    printaCSR(a, b, c, k);
+    for(int i = 0; i < k; i++){
+		cin >> m[i].row >> m[i].col >> m[i].dado;
+	}
+    ordenaMatrix(m, k);
+    cout << endl;
+    for(int i = 0; i < k; i++){
+        a[i] = m[i].dado;
+        b[i] = m[i].col;
+        c[m[i].row + 1]++;
+        
+        cout << m[i].row << " " << m[i].col << " " << m[i].dado << endl;
+    }
+    //printaCSR(a, b, c, k);
 
 
     return 0;
@@ -40,4 +51,19 @@ void printaCSR(int a[], int b[], int c[], int k)
         cout << c[i] << " ";
     }
     cout << endl;
+}
+void ordenaMatrix(matrix a[], int k)
+{
+    for(int i = 0; i < k; i++)
+    {
+        for(int j = 0; j < k; j++)
+        {
+            if(a[i].row < a[j].row && a[i].col < a[j].col)
+            {
+                matrix aux = a[i];
+                a[i] = a[j];
+                a[j] = aux;
+            }
+        }
+    }
 }
